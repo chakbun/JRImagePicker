bd5f9e5d4114e28afbc38acfd4f28be5b2c94f34
chakbun <cheungjaben@hotmail.com>
2015-01-14 14:50:23 +0800

    first commit

---
+++ JRImagePicker.h
@@ -0,0 +1,19 @@
+//
+//  JRImagePicker.h
+//  CameraLapTimer
+//
+//  Created by Jaben on 15-1-14.
+//  Copyright (c) 2015年 xpg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^JRImagePickerImageBlock)(UIImage *image);
+
+@interface JRImagePicker : NSObject
+
++ (instancetype)shareManager;
+- (void)loadImageFromiPhoneCamera:(UIViewController *)controller completionHandler:(JRImagePickerImageBlock)completed;
+- (void)loadImageFromiPhonePhotoLibrary:(UIViewController *)controller completionHandler:(JRImagePickerImageBlock)completed;
+- (void)loadImageFromiPhonePhotoAlbum:(UIViewController *)controller completionHandler:(JRImagePickerImageBlock)completed;
+@end
---
+++ JRImagePicker.m
@@ -0,0 +1,104 @@
+//
+//  JRImagePicker.m
+//  CameraLapTimer
+//
+//  Created by Jaben on 15-1-14.
+//  Copyright (c) 2015年 xpg. All rights reserved.
+//
+
+#import "JRImagePicker.h"
+
+@interface JRImagePicker ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate> {
+    UIImagePickerController *imagePicker;
+}
+
+@property (nonatomic, strong) UIViewController *rootController;
+@property (nonatomic, strong) JRImagePickerImageBlock imageBlock;
+
+@end
+
+@implementation JRImagePicker
+
++ (instancetype)shareManager {
+    static JRImagePicker *shareManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shareManager =[[JRImagePicker alloc] init];
+    });
+    return shareManager;
+}
+
+#pragma mark --Public
+
+- (void)loadImageFromiPhoneCamera:(UIViewController *)controller completionHandler:(JRImagePickerImageBlock)completed {
+    [self loadImagePickerOnController:controller type:UIImagePickerControllerSourceTypeCamera completionHandler:completed];
+}
+
+- (void)loadImageFromiPhonePhotoLibrary:(UIViewController *)controller completionHandler:(JRImagePickerImageBlock)completed {
+    [self loadImagePickerOnController:controller type:UIImagePickerControllerSourceTypePhotoLibrary completionHandler:completed];
+}
+
+- (void)loadImageFromiPhonePhotoAlbum:(UIViewController *)controller completionHandler:(JRImagePickerImageBlock)completed {
+    [self loadImagePickerOnController:controller type:UIImagePickerControllerSourceTypeSavedPhotosAlbum completionHandler:completed];
+}
+
+#pragma mark --Misc
+
+- (void)loadImagePickerOnController:(UIViewController *)controller type:(UIImagePickerControllerSourceType)type completionHandler:(JRImagePickerImageBlock)completed {
+    self.rootController = controller;
+    self.imageBlock = completed;
+    [self loadImagePick:type];
+}
+
+- (void)loadImagePick:(UIImagePickerControllerSourceType)sourceType {
+    
+    if (!imagePicker) {
+        imagePicker = [[UIImagePickerController alloc] init];
+        imagePicker.delegate = self;
+    }
+    
+    switch (sourceType) {
+        case UIImagePickerControllerSourceTypeCamera:{
+            if ([UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceRear] || [UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceFront]) {
+                [imagePicker setSourceType:sourceType];
+                [imagePicker setAllowsEditing:NO];
+                [self.rootController presentViewController:imagePicker animated:YES completion:^{}];
+            }
+            break;
+        }
+        case UIImagePickerControllerSourceTypeSavedPhotosAlbum:{
+            [imagePicker setSourceType:sourceType];
+            [imagePicker setAllowsEditing:NO];
+            [self.rootController presentViewController:imagePicker animated:YES completion:^{}];
+            break;
+        }
+        case UIImagePickerControllerSourceTypePhotoLibrary:{
+            [imagePicker setSourceType:sourceType];
+            [imagePicker setAllowsEditing:NO];
+            [self.rootController presentViewController:imagePicker animated:YES completion:^{}];
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+
+#pragma mark --ImagePickerController Delegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSLog(@"============ didFinishPickingMediaWithInfo ============");
+    NSLog(@"info : %@",[info description]);
+    [picker dismissViewControllerAnimated:YES completion:^{
+        UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+        if (self.imageBlock) {
+            self.imageBlock(image);
+        }
+    }];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    NSLog(@"============ imagePickerControllerDidCancel ============");
+    [picker dismissViewControllerAnimated:YES completion:^{}];
+}
+@end
